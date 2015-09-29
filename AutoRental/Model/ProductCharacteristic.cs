9b45dee49b53696d44aa2a08d1f9c8d1d61f6bf9
shakhandrew <shakhandrew95@gmail.com>
2015-09-29 18:53:42 +0300

    Characteristic for product

---
+++ AutoRental/Model/ProductCharacteristic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoRental.Model
+{
+    class ProductCharacteristic
+    {
+        public Product ProductForCharacteristic { get; set; }
+        public Characteristic CharacteristicForProduct { get; set; }
+        public String Value { get; set; }
+    }
+}
