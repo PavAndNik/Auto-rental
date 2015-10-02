455e5dc518b139919402149b99b3d6311c4e570f
shakhandrew <shakhandrew95@gmail.com>
2015-10-02 16:02:10 +0300

    ServicesProduct

---
+++ AutoRental/Services/ServicesProduct.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoRental.Services
+{
+    class ServicesProduct
+    {
+        private List<Product> ListOfProduct = new List<Product>();
+        public void AddProduct(Product NewProduct)
+        {
+            NewProduct.Id = ListOfProduct.Last<Product>().Id+1;
+            ListOfProduct.Add(NewProduct);
+        }
+        public Product GetProduct(string ID)
+        {
+            foreach(Product p in ListOfProduct)
+            {
+                if (p.Id == ID)
+                    return p;
+            }
+            return null;
+        }
+        public void DeleteProduct(string ID)
+        {
+            foreach (Product p in ListOfProduct)
+            {
+                if (p.Id == ID)
+                    ListOfProduct.Remove(p);
+            }
+        }
+        public void UpdateProduct(Product P)
+        {
+            int index = ListOfProduct.IndexOf(P);
+            if (index != -1)
+            {
+                ListOfProduct[index] = P;
+            }
+            else AddProduct(P);
+        }
+    }
+}
