1e04661bafd8138b0c585025d91f9c2c86216991
labovkin-nikolay <labovkin.nikolay@gmail.com>
2015-10-06 15:28:12 +0300

    1

---
+++ AutoRental/Model/ProductCharacteristic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoRental.Model
+{
+    class ProductCharacteristic
+    {
+        public Product Product { get; set; }
+        public Characteristic Characteristic { get; set; }
+        public String Value { get; set; }
+    }
+}
